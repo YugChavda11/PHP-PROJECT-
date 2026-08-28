cb04bba86fcf3787ebfcaa8fb2ef128ae7ae0492
jagrutgajera126439 <jagrutgajera.126439@marwadiuniversity.ac.in>
2026-08-29 01:25:18 +0530

    transactions.php

--- transactions.php
+++ transactions.php
@@ -1,5 +1,5 @@
 <?php
-// transactions.php - Full Transactions Management with Notifications Bell
+// Transactions.php - Full Transactions Management with Notifications Bell
 session_start();
 require_once __DIR__ . '/config/database.php';
 require_once __DIR__ . '/includes/auth_check.php';
